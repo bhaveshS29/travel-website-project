809bff70bcac777111c663e3eaf4577832c38db0
Bhavesh Sharma <96629217+bhaveshS29@users.noreply.github.com>
2022-05-27 18:53:52 +0530

    Add files via upload

---
+++ travel website project/assets/js/owl_crsl_my.js
@@ -0,0 +1,51 @@
+$('.world_trism').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('.sponser').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+$('.comment').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
